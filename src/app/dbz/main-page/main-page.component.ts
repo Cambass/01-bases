93305402bc03ebe51b28c5ff5c19acf45e00f5a5
cferro <cferro@bancolombia.com.co>
2022-05-20 15:44:24 -0500

    ngModel

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
@@ -7,9 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent {
 
-  agregar(){
+  personaje: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegueta',
+      poder: 10000
+    }
+  ];
 
-    console.log("Holi")
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  };
+
+  agregar(){
+    this.personaje.push(this.nuevo);
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    };
+  }
+
+  cambiarNombre(event: any){
+
   }
 
 }
